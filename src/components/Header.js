c1e326a8f1dca171624098639d39de25b843d3e1
Nwankwo Henry Chiemerie <49980451+Kozino@users.noreply.github.com>
2026-05-05 14:18:32 +0300

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>AfriSend</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
